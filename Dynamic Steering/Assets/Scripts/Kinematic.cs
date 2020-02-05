369b50d9099c56a22df20184466da7255b858bcd
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-02-05 15:04:32 -0700

    Changed Seperation to allow for other targets

--- Dynamic Steering/Assets/Scripts/Kinematic.cs
+++ Dynamic Steering/Assets/Scripts/Kinematic.cs
@@ -10,13 +10,13 @@ public class Kinematic : MonoBehaviour
     public float angularVelocity; // in degrees
     public GameObject target;
     public float maxSpeed = 40f;
-    public bool seek;
-    public bool arrive;
-    public bool align;
-    public bool lookWhereGoing;
-    public bool face;
     public dynamicSteering control;
-
+    public string movementType;
+    public string rotationType;
+    // Only For Seperation or path following
+    public GameObject[] Obstacles;
+    // When object is seperating do not provide any other control
+    bool seperating;
     // Update is called once per frame
     void Update()
     {
@@ -27,73 +27,136 @@ public class Kinematic : MonoBehaviour
         transform.eulerAngles += new Vector3(0, angularVelocity * Time.deltaTime, 0);
         //dynamicSteering steering = new Seek();
         // control to switch to proper steering behavior
-        if (!arrive)
+        if (Obstacles.Length > 0)
         {
-            Seek mySeek = new Seek();
-            mySeek.ai = this;
-            // if seek is false set seek property on class to false to activate flee
-            if (!seek)
-                mySeek.seek = false;
+            Seperation seperation = new Seperation();
+            seperation.targets = Obstacles;
+            seperation.ai = this;
+            SteeringOutput seperateForce = seperation.GetSteering();
+            // check to see if steering is greater than zero and lock out control from other steering
+            if (seperateForce.linear.magnitude > 0)
+                seperating = true;
             else
+                seperating = false;
+            linearVelocity += seperateForce.linear * Time.deltaTime;
+        }
+        switch (movementType)
+        {
+            case "seek":
+                Seek mySeek = new Seek();
+                mySeek.ai = this;
+                // if seek is false set seek property on class to false to activate flee
                 mySeek.seek = true;
-            mySeek.target = target;
-            SteeringOutput steering = mySeek.GetSteering();
-            linearVelocity += steering.linear * Time.deltaTime;
-            angularVelocity += steering.angular * Time.deltaTime;
-            if (linearVelocity.magnitude > maxSpeed)
-            {
-                linearVelocity.Normalize();
-                linearVelocity *= maxSpeed;
-            }
-        } else
-        {
-            Arrive myArrive = new Arrive();
-            myArrive.ai = this;
-            myArrive.target = target;
-            SteeringOutput steering = myArrive.GetSteering();
-          
-            linearVelocity += steering.linear * Time.deltaTime;
-            angularVelocity += steering.angular * Time.deltaTime;
+                mySeek.target = target;
+                SteeringOutput steeringSeek = mySeek.GetSteering();
+                if (!seperating)
+                    linearVelocity += steeringSeek.linear * Time.deltaTime;
+                if (linearVelocity.magnitude > maxSpeed)
+                {
+                    linearVelocity.Normalize();
+                    linearVelocity *= maxSpeed;
+                }
+                break;
 
+            case "flee":
+                Seek myFlee = new Seek();
+                myFlee.ai = this;
+                // if seek is false set seek property on class to false to activate flee
+                myFlee.seek = false;
+                myFlee.target = target;
+                SteeringOutput steeringFlee = myFlee.GetSteering();
+                if (!seperating)
+                    linearVelocity += steeringFlee.linear * Time.deltaTime;
+                if (linearVelocity.magnitude > maxSpeed)
+                {
+                    linearVelocity.Normalize();
+                    linearVelocity *= maxSpeed;
+                }
+                break;
+
+            case "arrive":
+                Arrive myArrive = new Arrive();
+                myArrive.ai = this;
+                myArrive.target = target;
+                SteeringOutput steeringArrive = myArrive.GetSteering();
+                if (!seperating)
+                    linearVelocity += steeringArrive.linear * Time.deltaTime;
+                break;
+            case "pursue":
+                Pursue myPursue = new Pursue();
+                myPursue.ai = this;
+                myPursue.target = target;
+                SteeringOutput steeringPursue = myPursue.GetSteering();
+                if (!seperating)
+                    linearVelocity += steeringPursue.linear * Time.deltaTime;
+                if (linearVelocity.magnitude > maxSpeed)
+                {
+                    linearVelocity.Normalize();
+                    linearVelocity *= maxSpeed;
+                }
+                break;
+            case "evade":
+                Pursue myEvade = new Pursue();
+                myEvade.ai = this;
+                myEvade.target = target;
+                // This changes the seek flag in the parent Seek class of Pursue, sending it the flee vector instead
+                myEvade.seek = false;
+                SteeringOutput steeringEvade = myEvade.GetSteering();
+                if (!seperating)
+                    linearVelocity += steeringEvade.linear * Time.deltaTime;
+                if (linearVelocity.magnitude > maxSpeed)
+                {
+                    linearVelocity.Normalize();
+                    linearVelocity *= maxSpeed;
+                }
+                break;
+            default:
+                // provide no input
+                break;
+            // If obstacles have been provided, return steering to seperate from them
+            
         }
-        if (align)
+
+        switch (rotationType)
         {
-            Align myAlign = new Align();
-            myAlign.ai = this;
-            myAlign.target = target;
-            SteeringOutput steering = myAlign.GetSteering();
-            if (steering != null)
-            {
-                linearVelocity += steering.linear * Time.deltaTime;
-                angularVelocity += steering.angular * Time.deltaTime;
-            }
-        }
-        if (lookWhereGoing && !align && !face)
-        {
-            LookWhereGoing myLook = new LookWhereGoing();
-            myLook.ai = this;
-            myLook.target = target;
-            SteeringOutput steering = myLook.GetSteering();
-            if (steering != null)
-            {
-                linearVelocity += steering.linear * Time.deltaTime;
-                angularVelocity += steering.angular * Time.deltaTime;
-            } else
-            {
-                Debug.Log("Returning Null");
-            }
-        }
-        if (!lookWhereGoing && !align && face)
-        {
-            Face myFace = new Face();
-            myFace.ai = this;
-            myFace.target = target;
-            SteeringOutput steering = myFace.GetSteering();
-            if (steering != null)
-            {
-                linearVelocity += steering.linear * Time.deltaTime;
-                angularVelocity += steering.angular * Time.deltaTime;
-            }
+            case "face":
+                Face myFace = new Face();
+                myFace.ai = this;
+                myFace.target = target;
+                SteeringOutput steeringFace = myFace.GetSteering();
+                if (steeringFace != null)
+                {
+                //    linearVelocity += steering.linear * Time.deltaTime;
+                    angularVelocity += steeringFace.angular * Time.deltaTime;
+                }
+
+                break;
+            case "align":
+                Align myAlign = new Align();
+                myAlign.ai = this;
+                myAlign.target = target;
+                SteeringOutput steeringAlign = myAlign.GetSteering();
+                if (steeringAlign != null)
+                {
+                    //linearVelocity += steering.linear * Time.deltaTime;
+                    angularVelocity += steeringAlign.angular * Time.deltaTime;
+                }
+                break;
+
+            case "look":
+                LookWhereGoing myLook = new LookWhereGoing();
+                myLook.ai = this;
+                myLook.target = target;
+                SteeringOutput steeringLook = myLook.GetSteering();
+                if (steeringLook != null)
+                {
+                    //linearVelocity += steering.linear * Time.deltaTime;
+                    angularVelocity += steeringLook.angular * Time.deltaTime;
+                }
+                break;
+            default:
+                //provide no input
+                break;
         }
     }
 }
